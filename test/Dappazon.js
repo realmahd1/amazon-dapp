dc786721441b9f6324d87714c39ac263cc71b905
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2024-07-10 20:42:22 +0330

    feat(Dappazon): create contract

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,24 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer, buyer;
 
+  beforeEach(async () => {
+    // Setup Accounts
+    [deployer, buyer] = await ethers.getSigners();
+    // Deploy contract
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    dappazon = await Dappazon.deploy();
+  })
+  describe("Deployment", () => {
+    it("Sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
 })
