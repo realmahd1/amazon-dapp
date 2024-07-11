93f601045339e87593a36f551c5bc94bf19ee4bd
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2024-07-11 10:38:25 +0330

    feat(Dappazon): list products

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -3,8 +3,54 @@ pragma solidity ^0.8.9;
 
 contract Dappazon {
     address public owner;
+    mapping(uint256 => Item) public items;
+
+    event List(string name, uint256 cost, uint256 quantity);
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
 
     constructor() {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this method");
+        _;
+    }
+
+    // List products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        // Create Item struct
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+
+        // Save Item Struct to blockchain
+        items[_id] = item;
+
+        // Emit an event
+        emit List(_name, _cost, _stock);
     }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -8,6 +8,13 @@ const tokens = (n) => {
 describe("Dappazon", () => {
   let dappazon;
   let deployer, buyer;
+  const ID = 1
+  const NAME = "Shoes"
+  const CATEGORY = "Clothing"
+  const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+  const COST = tokens(1)
+  const RATING = 4
+  const STOCK = 5
 
   beforeEach(async () => {
     // Setup Accounts
@@ -16,9 +23,44 @@ describe("Dappazon", () => {
     const Dappazon = await ethers.getContractFactory("Dappazon");
     dappazon = await Dappazon.deploy();
   })
+
   describe("Deployment", () => {
     it("Sets the owner", async () => {
       expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
+
+  describe('Listing', () => {
+    let tranasction;
+
+    beforeEach(async () => {
+      tranasction = await dappazon.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+
+      await tranasction.wait();
+    })
+
+    it("Returns item attributes", async () => {
+      const item = await dappazon.items(ID);
+
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    })
+
+    it("Emits List event", () => {
+      expect(tranasction).to.emit(dappazon, "List");
     })
   })
 })
