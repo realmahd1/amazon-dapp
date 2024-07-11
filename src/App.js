a50010a2873eabcb640b6d0e6659c02c6d98866c
M.Mahdi Saeidi <M.Mahdisaeidi99@gmail.com>
2024-07-11 13:47:30 +0330

    feat(navigation): add navigation component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
 
 // Components
 import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
 
 // ABIs
-import Dappazon from './abis/Dappazon.json'
 
 // Config
-import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
-
+      <Navigation account={account} setAccount={setAccount} />
       <h2>Welcome to Dappazon</h2>
 
     </div>
